81c108b148dc359f585c69fcb5c80fa1f0d7595d
DivyaPrakash <divyaprakash002@gmail.com>
2020-09-05 09:44:58 +0530

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { PhotoService, Photo } from '../service/photo.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  public photos: Photo[] = [];
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+    ) { }
 
-  constructor() {}
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
+
+  public addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  public async showActionSheet(photo: Photo, position: number){
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
 
 }
